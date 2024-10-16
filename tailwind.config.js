9acc952444ae1449bf4e07181c8099e1f92b93f8
Archit-Bhadange <archit.bhadange@gmail.com>
2024-10-16 13:48:01 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        blink: 'blink 2s infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%': { opacity: 1 },
+          '50%': { opacity: 0.5 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [
+    // require('@tailwindcss/forms'),
+  ],
+}
